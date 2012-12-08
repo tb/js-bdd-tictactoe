22445b8d14b317e08140ca671384d15f7e1e6035
Tomasz Bak <t.bak@selleo.com>
2012-12-08 11:46:18 +0100

    clear tests impl

--- spec/board_spec.js
+++ spec/board_spec.js
@@ -4,46 +4,6 @@ describe('Board',function(){
 
   beforeEach(function() {
     loadFixtures('board1.html');
-
-    $container = $('.board1').tictactoe({
-      onFinish: function(winner) {
-        alert('Good job' + winner);
-      }
-    });
-    $tictactoe = $container.data('tictactoe');
-    $board = $tictactoe.getBoard();
-
-    _.extend($board, {
-      click: function() {
-        _.each(arguments, function(index){
-          $board.find('[data-cell='+index+']').trigger('click');
-        });
-      }
-    });
   });
 
-  it('should be table with have 9 cells',function(){
-    expect($board.find('[data-cell]').length).toBe(9);
-  });
-
-  it('should have data-tictactoe',function(){
-    expect($container).toHaveData('tictactoe');
-  });
-
-  it('should write symbol to board',function(){
-    $board.click(1);
-    expect($board.find('[data-cell=1]')).toHaveClass('xSelectedField');
-  });
-
-  it('should display winner after finish',function(){
-    spyOn(window, 'alert');
-    $board.click(1, 4, 2, 5, 3, 6, 7);
-    expect(window.alert).toHaveBeenCalledWith('Good job x!"');
-  });
-
-  it('should not display winner if game is not finish',function(){
-    spyOn(window, 'alert');
-    $board.click(1, 2, 3, 4, 5, 6);
-    expect(window.alert).wasNotCalled();
-  });
 });
